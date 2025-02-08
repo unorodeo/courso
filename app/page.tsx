f386ccddb56db5f2a07c96bc4ec08bdeb537bfbd
enoabasi <enoabasidream@gmail.com>
2025-02-08 18:51:01 +0100

    feat: setup component library

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="p-4">
       <section>
         <h1 className="text-3xl font-medium">Hello world</h1>
+        <Button>Get Started</Button>
       </section>
     </main>
   );
